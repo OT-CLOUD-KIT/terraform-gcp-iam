c6cab5b4aaaf9b2020f5c8a2dba82f3a677469b8
shreya-jaiswal37 <156057205+shreya-jaiswal37@users.noreply.github.com>
2025-03-12 13:15:31 +0530

    Update main.tf

--- example/service_account_mngmnt/main.tf
+++ example/service_account_mngmnt/main.tf
@@ -1,16 +1,3 @@
-# resource "google_service_account" "service_accounts" {
-#   for_each     = var.service_accounts
-#   account_id   = each.key
-#   display_name = each.value.display_name
-# }
-
-# resource "google_project_iam_member" "sa_roles" {
-#   for_each = var.service_accounts
-#   project  = var.project_id
-#   role     = each.value.role
-#   member   = "serviceAccount:${google_service_account.service_accounts[each.key].email}"
-# }
-
 resource "google_service_account" "service_accounts" {
   for_each     = var.service_accounts
   account_id   = each.key
